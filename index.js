0fc835c41f3677b7d8c19b3b58f0813b31810ffa
Julian Schoepf <ferlojulian@hotmail.com>
2022-01-20 12:38:02 +0100

    Random Color Generator 31x9#

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
 const color = randomColor({
-  hue: process.argv[2],
   luminosity: process.argv[3],
+  hue: process.argv[2],
 });
 
 console.log(
